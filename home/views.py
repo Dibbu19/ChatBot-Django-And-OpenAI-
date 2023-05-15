e227568c2a4e3276d53ccf4ca045437c6584625d
Divyansh <divyanshchauhan953@gmail.com>
2023-05-16 03:55:56 +0530

    ChatBot

--- home/views.py
+++ home/views.py
@@ -3,11 +3,7 @@ from django.http import JsonResponse
 import os
 import openai
 
-<<<<<<< HEAD
-openai.api_key = ""  # type your OpenAI Secret Key Here
-=======
-openai.api_key = "" //type your OpenAI secret key here
->>>>>>> a6d22755ec50ee4410784308ecacab98949a02b8
+openai.api_key = ""  # type your OpenAI secret key here
 
 
 # Create your views here.
