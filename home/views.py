5c8192725b2c980cf39947d7897a43210373ce4d
Divyansh <divyanshchauhan953@gmail.com>
2023-05-16 04:08:06 +0530

    ChatBot

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import os
+import openai
+
+openai.api_key = ""  # type your OpenAI secret key here
+
+
+# Create your views here.
+
+
+def chat(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def chatAPI(request):
+    if request.method == "POST":
+        prompt = request.POST["prompt"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        print(response)
+        return JsonResponse(response)
+    return HttpResponse("Bad Request")
