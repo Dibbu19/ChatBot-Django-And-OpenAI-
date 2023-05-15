f220864c7711fff4abb93b510aa7d9b7eaa37515
Divyansh <divyanshchauhan953@gmail.com>
2023-05-16 03:30:01 +0530

    ChatBot Ready

--- home/urls.py
+++ home/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import chat, about
+from home.views import chat, about, chatAPI
 
 urlpatterns = [
     path('chat', chat, name='home'),
-    path('about', about, name='about')
+    path('about', about, name='about'),
+    path('api', chatAPI, name='chatAPI')
 
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import os
+import openai
+
+openai.api_key = "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"
+
 
 # Create your views here.
 
@@ -12,4 +18,17 @@ def about(request):
 
 
 def chatAPI(request):
-    return HttpResponse("This works")
+    if request.method == "POST":
+        prompt = request.POST["prompt"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        print(response)
+        return JsonResponse(response)
+    return HttpResponse("Bad Request")
