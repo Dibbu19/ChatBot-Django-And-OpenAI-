6c136153f2e04909a219a2733956427724893f16
Divyansh <divyanshchauhan953@gmail.com>
2023-05-16 03:36:42 +0530

    ChatBot Ready

--- home/urls.py
+++ home/urls.py
@@ -20,7 +20,6 @@ from home.views import chat, about, chatAPI
 
 urlpatterns = [
     path('chat', chat, name='home'),
-    path('about', about, name='about'),
     path('api', chatAPI, name='chatAPI')
 
 
